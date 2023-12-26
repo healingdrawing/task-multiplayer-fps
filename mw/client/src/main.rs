b13899bcdfe8e3f26313772f8d5564c90a1c92b6
healingdrawing <healingdrawing@gmail.com>
2023-12-26 10:36:24 +0200

    not nice. all window boolean settings are ignored

--- mw/client/src/main.rs
+++ mw/client/src/main.rs
@@ -1,5 +1,22 @@
-use bevy::prelude::*;
+use bevy::prelude::* ;
 
 fn main() {
-  App::new().add_plugins(DefaultPlugins).run();
+  App::new().add_plugins(DefaultPlugins.set(
+    WindowPlugin {
+      primary_window: Some(
+        Window {
+          resizable: false,
+          resolution: (1000., 1000.).into(),
+          title: "asspain + headache + brainfuck".into(),
+          enabled_buttons: bevy::window::EnabledButtons{
+            maximize: false,
+            ..default()
+          },
+          ..default()
+        }
+      ),
+      ..default()
+    }
+  ))
+  .run();
 }
