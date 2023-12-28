67a72099bd06a61a33a18a4effef0b0a7900c1b4
healingdrawing <healingdrawing@gmail.com>
2023-12-28 04:27:26 +0200

    add todo comments

--- mw/client/src/main.rs
+++ mw/client/src/main.rs
@@ -10,7 +10,23 @@ use gui::fps::show_fps::show_fps_ui;
 mod keyboard;
 use keyboard::activity::listen_keys;
 
+/// it is a client for the server
 fn main() {
+  
+  // placeholder:
+  // for request the server ip:port from user terminal input, and store it in the variable
+  // only numbers , dots and colon allowed, otherwise print error and loop again to collect the ip:port
+
+  // placehoder:
+  // for request the user name from user terminal input, and store it in the variable
+  // only the english letters and the numbers are allowed, otherwise print error message and loop again to collect the name
+
+  // placehoder:
+  // after collecting the ip:port and the name, try to connect to the server
+  // if connection is not successful/fail, then print error message and loop again to collect the ip:port and the name
+
+  // if connection is successful, then start the game
+
   App::new()
   .add_plugins(
     (
@@ -44,4 +60,6 @@ fn main() {
     )
   )
   .run();
+
+  // placeholder for game over, just close the window and/or exit. Not sure how make it the best way
 }
